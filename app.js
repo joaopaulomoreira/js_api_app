976afd7507de64e4f2d1849f1205252816860674
Joao Paulo Moreira Carvalho <labdev18@DTI-01.gruposolucao.com>
2020-11-10 18:22:20 -0300

    Mongodb install, require and connect

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 8000
 
+app.use(express.json())
+mongoose.connect('mongodb://localhost:27017/api_app', {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true}).then(() => {
+
+    console.log("Conexão ao banco realizada com sucesso")
+
+  } ).catch(() => {
+
+    console.log("Erro: conexão ao banco falhou")
+
+  })
+
 app.get('/', (req, res) => {
-  res.send('Hello World App API Imersao!')
+  res.json({name: "Joao"})
 })
 
 app.listen(port, () => {
